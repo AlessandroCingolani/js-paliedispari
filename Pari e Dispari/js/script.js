db4b0d71122b486b134020c2f9177eebd417f5d5
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-15 17:36:03 +0200

    [add]: result and validitation

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,24 +1,45 @@
+let wordCheck = false;
+let numberCheck = false;
+const output = document.getElementById('output');
+let playerNumber 
+let playerPariDispari
 
+// convalid pari or dispari
+do{
+  playerPariDispari = prompt('Scegli pari o dispari').toLowerCase();
+  if (playerPariDispari === 'pari' || playerPariDispari === 'dispari'){
+   wordCheck = true;
+ }else {
+   alert('Errore inserire campi corretti!')
+  }
+  }while(!wordCheck)
 
+  console.log(playerPariDispari);
 
+// convalid player number
+do{
+  playerNumber = parseInt(prompt('Scegli numero da 1 a 5'));
+  if (playerNumber > 0 && playerNumber < 6 ){
+   numberCheck = true;
+ }else {
+   alert('Errore inserire campi corretti!')
+  }
+}while(!numberCheck)
 
+console.log(playerNumber);
 
+let computerNumber = randomGen()
 
+let result = pariDispari(playerNumber,computerNumber)
 
-
-
-
-
-
-
-
-
+console.log('totale ' + result);
+console.log('numero computer ' + computerNumber);
 
 /**
  * somma numeri e controlla se è paro o disparo
  * @param {number} playerNumber 
  * @param {number} computerNumber
- *  @returns number 
+ * @returns number 
  */
 function pariDispari(playerNumber,computerNumber){
   let sum = playerNumber + computerNumber;
@@ -30,10 +51,8 @@ function pariDispari(playerNumber,computerNumber){
 
 /**
  * estrae numero random per computer
- * @param {number} min 
- * @param {number} max 
  * @returns number
  */
-function randomGen(min,max) {
-  return Math.floor(Math.random() * (max - min +1))+ min;
+function randomGen() {
+  return Math.floor(Math.random() * (5 - 1 +1))+ 1;
 }
