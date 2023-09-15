3f0ad40bb1efd810e4532d92d0d2bec570b12995
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-15 15:20:10 +0200

    [add]:reverse loop

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -11,4 +11,10 @@ let wordCheck = false;
 // }
 
 const insertedWord = prompt('Inserire una parola con almeno 3 caratteri');
+let reverseWord=''
 
+for (let i = insertedWord.length -1 ; i >= 0 ;i--){
+  reverseWord += insertedWord[i]
+}
+
+console.log(reverseWord);
